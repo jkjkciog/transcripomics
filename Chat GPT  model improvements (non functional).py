5e38988f3d131df13c13fe3ef3457a750fb87226
jkjkciog <38109477+jkjkciog@users.noreply.github.com>
2023-02-21 23:09:55 -0500

    commit

--- Chat GPT  model improvements (non functional).py
+++ Chat GPT  model improvements (non functional).py
@@ -1,53 +1,51 @@
 from Bio import SeqIO
 
-def find_orf(sequence, start_pos, stop_codons):
-    start_codon = "ATG"
-    for i in range(start_pos, len(sequence), 3):
-        codon = sequence[i:i+3]
-        if codon == start_codon:
-            orf = codon
-            for j in range(i+3, len(sequence), 3):
-                codon = sequence[j:j+3]
-                if codon in stop_codons:
-                    return orf, j
-                orf += codon
-            return orf, len(sequence)
-    return "", start_pos
+def find_stop_codon(seq, start, stop_codons):
+    """Find the next stop codon in the given sequence."""
+    for i in range(start, len(seq), 3):
+        codon = seq[i:i+3]
+        if codon in stop_codons:
+            return i
+    return None
 
-def find_orf4(sequence):
-    start = sequence.find("ATG")
-    if start > -1:
-        stop_codons = ["TAA", "TAG", "TGA"]
-        stop = len(sequence)
-        for stop_codon in stop_codons:
-            temp_stop = sequence.find(stop_codon, start)
-            if temp_stop > -1 and temp_stop < stop:
-                stop = temp_stop
-        return sequence[start:stop], stop
-    return "", 0
+def find_orf(sequence, start_codon="ATG", stop_codons=["TAA", "TAG", "TGA"]):
+    """Find the longest ORF in the given sequence."""
+    longest_orf = ""
+    for i in range(len(sequence)):
+        if sequence[i:i+3] == start_codon:
+            stop = find_stop_codon(sequence, i+3, stop_codons)
+            if stop is not None:
+                orf = sequence[i:stop]
+                if len(orf) > len(longest_orf):
+                    longest_orf = orf
+    return longest_orf
 
-def process_record(seq_record):
-    print(seq_record.id)
-    print(repr(seq_record.seq))
-    print(len(seq_record))
-    orf, stop = find_orf4(str(seq_record))
-    while stop < len(seq_record):
-        orf, stop = find_orf(str(seq_record), stop, ["TAA", "TAG", "TGA"])
-        if len(orf) > 0:
-            print("ORF:", orf)
+def find_longest_shortest(records):
+    """Find the longest and shortest records in the given list of records."""
+    longest_record = None
+    shortest_record = None
+    for record in records:
+        if longest_record is None or len(record.seq) > len(longest_record.seq):
+            longest_record = record
+        if shortest_record is None or len(record.seq) < len(shortest_record.seq):
+            shortest_record = record
+    return longest_record, shortest_record
+
+def main():
+    records = list(SeqIO.parse("dna2.fasta", "fasta"))
+    longest_record, shortest_record = find_longest_shortest(records)
+    print(f"The longest record is {longest_record.id} with length {len(longest_record.seq)}")
+    print(f"The shortest record is {shortest_record.id} with length {len(shortest_record.seq)}")
+    for i, record in enumerate(records):
+        print(f"Record number: {i+1}")
+        print(f"ID: {record.id}")
+        print(f"Sequence: {record.seq}")
+        orf = find_orf(str(record.seq))
+        if orf:
+            print(f"ORF: {orf}")
         else:
             print("No ORF found")
+        print()
 
-    print()
-
-records = list(SeqIO.parse("dna2.fasta", "fasta"))
-longest_record = max(records, key=lambda r: len(r.seq))
-shortest_record = min(records, key=lambda r: len(r.seq))
-print("Number of records: ", len(records))
-print("ID of longest record: ", longest_record.id)
-print("Length of longest record: ", len(longest_record))
-print("ID of shortest record: ", shortest_record.id)
-print("Length of shortest record: ", len(shortest_record))
-
-for seq_record in records:
-    process_record(seq_record)
+if __name__ == "__main__":
+    main()
