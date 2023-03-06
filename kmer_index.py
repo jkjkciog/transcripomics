f40c3730165ff969ebdf20a90b51f942e0815586
jkjkciog <38109477+jkjkciog@users.noreply.github.com>
2023-03-05 22:14:45 -0500

    commit

--- kmer_index.py
+++ kmer_index.py
@@ -55,3 +55,134 @@ class Index(object):
             hits.append(self.index[i][1])
             i += 1
         return hits
+    
+
+
+    import bisect
+
+
+def naive_2mm(p, t):
+    occurrences = []
+    for i in range(len(t) - len(p) + 1):  # loop over alignments
+        count_mismatch = 0
+        for j in range(len(p)):  # loop over characters
+            if t[i+j] != p[j]:  # compare characters
+                count_mismatch += 1
+        if count_mismatch <= 2:
+            occurrences.append(i)  # all chars matched; record
+    return occurrences
+
+
+def approximate_match(p, t, n):
+    segment_length = int(round(len(p) / (n + 1)))
+    all_matches = set()
+    p_idx = Index(t, segment_length)
+    idx_hits = 0
+    for i in range(n + 1):
+        start = i * segment_length
+        end = min((i + 1) * segment_length, len(p))
+        matches = p_idx.query(p[start:end])
+
+        # Extend matching segments to see if whole p matches
+        for m in matches:
+            idx_hits += 1
+            if m < start or m - start + len(p) > len(t):
+                continue
+
+            mismatches = 0
+
+            for j in range(0, start):
+                if not p[j] == t[m - start + j]:
+                    mismatches += 1
+                    if mismatches > n:
+                        break
+            for j in range(end, len(p)):
+                if not p[j] == t[m - start + j]:
+                    mismatches += 1
+                    if mismatches > n:
+                        break
+
+            if mismatches <= n:
+                all_matches.add(m - start)
+    return list(all_matches), idx_hits
+
+
+def approximate_match_subseq(p, t, n, ival):
+    segment_length = int(round(len(p) / (n + 1)))
+    all_matches = set()
+    p_idx = SubseqIndex(t, segment_length, ival)
+    idx_hits = 0
+    for i in range(n + 1):
+        start = i
+        matches = p_idx.query(p[start:])
+
+        # Extend matching segments to see if whole p matches
+        for m in matches:
+            idx_hits += 1
+            if m < start or m - start + len(p) > len(t):
+                continue
+
+            mismatches = 0
+
+            for j in range(0, len(p)):
+                if not p[j] == t[m - start + j]:
+                    mismatches += 1
+                    if mismatches > n:
+                        break
+
+            if mismatches <= n:
+                all_matches.add(m - start)
+    return list(all_matches), idx_hits
+
+
+# class Index is provided by instructor Ben Langmead
+class Index(object):
+    """ Holds a substring index for a text T """
+
+    def __init__(self, t, k):
+        """ Create index from all substrings of t of length k """
+        self.k = k  # k-mer length (k)
+        self.index = []
+        for i in range(len(t) - k + 1):  # for each k-mer
+            self.index.append((t[i:i+k], i))  # add (k-mer, offset) pair
+        self.index.sort()  # alphabetize by k-mer
+
+    def query(self, p):
+        """ Return index hits for first k-mer of p """
+        kmer = p[:self.k]  # query with first k-mer
+        i = bisect.bisect_left(self.index, (kmer, -1))  # binary search
+        hits = []
+        while i < len(self.index):  # collect matching index entries
+            if self.index[i][0] != kmer:
+                break
+            hits.append(self.index[i][1])
+            i += 1
+        return hits
+
+
+class SubseqIndex(object):
+    """ Holds a subsequence index for a text T """
+
+    def __init__(self, t, k, ival):
+        """ Create index from all subsequences consisting of k characters
+            spaced ival positions apart.  E.g., SubseqIndex("ATAT", 2, 2)
+            extracts ("AA", 0) and ("TT", 1). """
+        self.k = k  # num characters per subsequence extracted
+        self.ival = ival  # space between them; 1=adjacent, 2=every other, etc
+        self.index = []
+        self.span = 1 + ival * (k - 1)
+        for i in range(len(t) - self.span + 1):  # for each subseq
+            self.index.append((t[i:i + self.span:ival], i))  # add (subseq, offset)
+        self.index.sort()  # alphabetize by subseq
+
+    def query(self, p):
+        """ Return index hits for first subseq of p """
+        subseq = p[:self.span:self.ival]  # query with first subseq
+        i = bisect.bisect_left(self.index, (subseq, -1))  # binary search
+        hits = []
+        while i < len(self.index):  # collect matching index entries
+            if self.index[i][0] != subseq:
+                break
+            hits.append(self.index[i][1])
+            i += 1
+        return hits
