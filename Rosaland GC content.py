2cb656cfa1550b5d043f5866838c2fecb80d9e1e
jkjkciog <jaan@kolesnik.biz>
2023-04-02 17:10:42 -0400

    commit

--- Rosaland GC content.py
+++ Rosaland GC content.py
@@ -15,4 +15,16 @@ with open('rosalind_gc.txt', 'r') as f:
     print(f'GC content of {header}: {gc_content:.2f}%')
 
 
+with open('rosalind_gc.txt', 'r') as f:
+    for line in f:
+        if line.startswith('>'):  # if line is a header
+            gc_content = (seq.count('G') + seq.count('C')) / len(seq) * 100
+            print(f'GC content of {header}: {gc_content:.2f}%')
+            seq = ''  # reset sequence
+            header = line.strip()[1:]  # remove ">" and newline characters
+        else:
+            seq += line.strip()
 
+    # process the last sequence
+    gc_content = (seq.count('G') + seq.count('C')) / len(seq) * 100
+    print(f'GC content of {header}: {gc_content:.2f}%')
