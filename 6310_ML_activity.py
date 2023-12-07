70fe6b85cf64aa8392b9c43dd30a95bf07590b0c
jkjkciog <jaan@kolesnik.biz>
2023-12-06 20:43:43 -0500

    commit

--- 6310_ML_activity.py
+++ 6310_ML_activity.py
@@ -8,3 +8,17 @@ try:
 
 except: 
     print(f'Error importing libraries')
+
+column_name= ["pregnancies", "glucose", "blood_pressure", "skin_thickness", "insulin", "bmi", "diabetes_pedigree_function", "age", "outcome"]
+
+df = pd.read_csv("C:\\Users\\jaan\\Downloads\\6310_ML_activity.csv", names=column_name)
+
+
+print(df.shape)
+print(df.head())
+
+X=df.iloc[:,0:8]
+print(X.head())
+
+Y=df.iloc['class']
+print(Y.head())
